c17827cedf094fc920e1991c405682ce403a4f54
Adarsh-MR <adarshmr1998@gmail.com>
2018-02-16 20:30:38 +0000

    msm8956-common: Initial O sepolicy
    * WIP
    * move shell scripts to vendor
    * include legacy-sepolicy.mk
    
    Signed-off-by: Adarsh-MR <adarshmr1998@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -203,8 +203,9 @@ USE_OPENGL_RENDERER := true
 PROTOBUF_SUPPORTED := true
 
 # SELinux
-#include device/qcom/sepolicy/sepolicy.mk
-#BOARD_SEPOLICY_DIRS += $(VENDOR_PATH)/sepolicy
+include device/qcom/sepolicy/sepolicy.mk
+include device/qcom/sepolicy/legacy-sepolicy.mk
+BOARD_SEPOLICY_DIRS += $(VENDOR_PATH)/sepolicy
 
 # Wifi
 WPA_SUPPLICANT_VERSION      := VER_0_8_X
